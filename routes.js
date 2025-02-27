e1eb745c396c8cdb488743257720b3a840e1f273
Josephine Verol <99454751+jjoseph48@users.noreply.github.com>
2025-02-27 23:31:28 +0700

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,1 +1,42 @@
-// routes
+const { 
+  addBookHandler, 
+  getAllBooksHandler, 
+  getBookByIdHandler, 
+  editBookByIdHandler, 
+  deleteBookByIdHandler, 
+} = require('./handler');
+
+const routes = [
+  // Route untuk menambahkan buku
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  // Route untuk mendapatkan semua buku
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  // Route untuk mendapatkan buku berdasarkan ID
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookByIdHandler,
+  },
+  // Route untuk mengedit buku berdasarkan ID
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: editBookByIdHandler,
+  },
+  // Route untuk menghapus buku berdasarkan ID
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookByIdHandler,
+  },
+];
+
+module.exports = routes;
