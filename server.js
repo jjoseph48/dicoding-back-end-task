08431c7ef9a16a5efffdf7ed0395532ffa074d68
Josephine Verol <99454751+jjoseph48@users.noreply.github.com>
2025-02-27 23:45:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const init = async () => {
 
   await server.start();
 
-  console.log(`Server berjalan pada ${server.info.uri}');
+  console.log(`Server berjalan pada ${server.info.uri}`);
   };
 
   init();
